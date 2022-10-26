15b8dea3b8bc33473f9b96c3a78fcbc36756e4bf
reztsky <sultaninot@gmail.com>
2022-10-26 22:06:32 +0700

    Update Barang Stok Change To 0

--- app/Http/Requests/UpdateBarangRequest.php
+++ app/Http/Requests/UpdateBarangRequest.php
@@ -30,7 +30,7 @@ class UpdateBarangRequest extends FormRequest
             'keterangan_barang'=>'nullable',
             'harga'=>'required|numeric|min:1',
             'satuan'=>'required',
-            'stok'=>'required|numeric|min:1',
+            'stok'=>'required|numeric',
             'stok_minimal'=>'numeric',
             'stok_maksimal'=>'numeric',
         ];
